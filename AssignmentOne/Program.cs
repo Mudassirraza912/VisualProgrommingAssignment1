ab86f42d8c7e594ca5d606f97f843275f7a21d81
Mudassirraza912 <razamudassir912@gmail.com>
2023-04-01 23:07:11 +0500

    Solve more questions

--- AssignmentOne/Program.cs
+++ AssignmentOne/Program.cs
@@ -1,8 +1,10 @@
-﻿class AssignmentOne
+﻿using System;
+
+class AssignmentOne
 {
     static void Main(string[] args)
     {
-        QuestionSix();
+        QuestionSixteen();
     }
 
     static void QuestionOne()
@@ -103,8 +105,108 @@
             {
                 Console.WriteLine("InValid Input");
             }
-
         }
-
     }
+
+    static void QuestionSeven() {
+        int number = 0;
+        Console.WriteLine("Enter a number for printing table");
+        var num = Console.ReadLine();
+        if(int.TryParse(num, out number)) {
+            for (int i = 1; i <= 12; i++)
+            {
+                Console.WriteLine($"{num} x {i} = {int.Parse(num) * i} \n");
+            }
+        }else {
+            Console.WriteLine("Invalid input. Retry");
+            QuestionSeven();
+        }
+    }
+
+    static void QuestionTen() {
+        int currentYear = DateTime.Now.Year;
+        int count = 0;
+        while(count < 20) {
+            if(DateTime.IsLeapYear(currentYear)) {
+                Console.WriteLine(currentYear);
+                count++;
+            };
+            currentYear++;
+        }
+    }
+
+    static void QuestionEleven() {
+        int[] myArray = new int[] { 1, 2, 34, 5, 6 };
+        int largest = myArray[0];
+        for(int i =1; i < myArray.Length; i++) {
+            if (myArray[i] > largest) {
+                largest = myArray[i];
+            }
+        }
+        Console.WriteLine($"Largest number is in a list {largest}");
+    }
+
+    static void QuestionTwelve()
+    {
+        int[] list = new int[] { 1, 2, 3, 4, 5 };
+        string rev = "";
+        for (int i = list.Length - 1; i >= 0; i--)
+        {
+            rev += $" {list[i]},";
+        }
+        Console.WriteLine($"Reverse of list is {rev}");
+    }
+
+    static void QuestionThirteen() {
+        string[] list = new string[] { "Mudassir", "Haris", "Faraz", "Wirad"};
+        Console.WriteLine("Whom are you searching for?");
+        var item = Console.ReadLine();
+        string result = "";
+        for(int i=0; i < list.Length; i++) {
+            if(item == list[i]) {
+                Console.WriteLine($"{list[i]} is found in a list at {i} position");
+                return;
+            }
+        }
+        Console.WriteLine($"Oooops {item} is not in a list");
+    }
+
+    static void QuestionFourteen()
+    {
+        int[] list = new int[] { 1,2,3,4,5,6,7,8,9,10 };
+        string oddList = "";
+        for (int i =0; i < list.Length; i++)
+        {
+            if(i%2 != 0) {
+                oddList += $" {list[i]},";
+            }
+        }
+        Console.WriteLine($"Elements in Odd position in list is {oddList}");
+    }
+
+    static void QuestionFifteen()
+    {
+        int[] list = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int result = 0;
+        for (int i = 0; i < list.Length; i++)
+        {
+            result += list[i];
+        }
+        Console.WriteLine($"The sum of the list item is {result}");
+    }
+
+    static void QuestionSixteen() {
+        Console.WriteLine("Enter a word to check if it Palindrome or not");
+        var input = Console.ReadLine().ToLower();
+        string reverse = "";
+        for(int i= input.Length - 1; i >=0; i--) {
+            reverse += input[i];
+        };
+        if(reverse == input) {
+            Console.WriteLine($"{input} is Palindrome");
+        }else {
+            Console.WriteLine($"{input} is not Palindrome");
+        }
+    }
+
 }

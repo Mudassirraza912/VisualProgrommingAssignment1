849726ba82486e506da823260345e8e025915cff
Mudassirraza912 <razamudassir912@gmail.com>
2023-04-02 21:09:50 +0500

    WIP: 8 Question remaining

--- AssignmentOne/Program.cs
+++ AssignmentOne/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class AssignmentOne
 {
     static void Main(string[] args)
     {
-        QuestionSixteen();
+        QuestionThirty();
     }
 
     static void QuestionOne()
@@ -209,4 +210,147 @@ class AssignmentOne
         }
     }
 
+    // QuestionSeventeen
+    static void SumWithForLoop()
+    {
+        int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int sum = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            sum += numbers[i];
+        };
+        Console.WriteLine($"The sum with for-loop is {sum}");
+    }
+
+    static void SumWithWhileLoop()
+    {
+        int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int sum = 0;
+        int i = 0;
+        while (i < numbers.Length)
+        {
+            sum += numbers[i];
+            i++;
+        }
+        Console.WriteLine($"The sum with while-loop is {sum}");
+    }
+
+    static void SumWithRecurssion() {
+        List<int> list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        static int recurssion(List<int> numbers) {
+            if (numbers.Count == 0) {
+                return 0;
+            }else {
+                int numToAdd = numbers[numbers.Count - 1];
+                numbers.RemoveAt(numbers.Count - 1);
+                return recurssion(numbers) + numToAdd;
+            }
+        };
+        int sum = recurssion(list);
+        Console.WriteLine($"The sum with recurssion is {sum}");
+    }
+
+    static void QuestionTwenty() {
+        List<int> finalArray = new List<int>();
+        int num = 23456;
+        string str = num.ToString();
+        for(int i=0; i < str.Length; i++) {
+            Console.WriteLine($"i {str[i].GetType()}");
+            // we convert char to numeric value and cast to int befor add to list
+            finalArray.Add((int)Char.GetNumericValue(str[i]));
+            //finalArray.Insert(i, str[i]);
+        }
+        Console.WriteLine($"finalArray is {string.Join(", ", finalArray)}");
+    }
+
+    static void QuestionTwentyOne()
+    {
+        List<string> words = new List<string>{"Hello", "World", "in", "a", "frame" };
+        //First i find max length of word in a list
+        int maxLength = words.Max(word => word.Length);
+        // then i will create a horizontal line here with maxLength + 4 for top and bottom, here "+ 4" is used because every line i start with "* " and end with " *" 
+        string horizontalLine = new string('*', maxLength + 4);
+        // show top border
+        Console.WriteLine(horizontalLine);
+         //Then show content
+        foreach (string word in words)
+        {
+            int spacesToAdd = maxLength - word.Length;
+            string paddedWord = "* " + word + new string(' ', spacesToAdd) + " *";
+            Console.WriteLine(paddedWord);
+        }
+        //And Finally Bottom border thats the LF and CF.
+        Console.WriteLine(horizontalLine);
+    }
+
+    static void QuestionTwentyFour () {
+        int num;
+        Console.Write("Enter an integer: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"The cube of {num} is {num * num * num}");
+        QuestionTwentyFour();
+    }
+
+    static void QuestionTwentyFive() {
+        Console.WriteLine("Enter the number to print table till that number");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                int product = i * j;
+                Console.WriteLine($"{i} x {j} = {product}");
+            }
+            Console.WriteLine(" \n \n");
+        }
+    }
+
+    static void QuestionEight () {
+        Console.WriteLine("Prime numbers between 1 and 100:");
+        for (int i = 2; i <= 100; i++)
+        {
+            bool isPrime = true;
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+    }
+
+    static void QuestionTwentySeven() {
+        Console.WriteLine("Enter the number to check its Prime or not");
+        int i = Convert.ToInt32(Console.ReadLine());
+        bool isPrime = true;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime)
+        {
+            Console.WriteLine(i + " is prime number ");
+        }else {
+            Console.WriteLine(i + " is not a prime number ");
+        }
+        QuestionTwentySeven();
+    }
+
+    static void QuestionThirty() {
+        int[] list = new int[] { 1, 4, 5, 6, 7, 8, 34, 67 };
+        Console.WriteLine("The largest element in the array is: " + list.Max());
+    }
 }
